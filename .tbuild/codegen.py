c85143094cf17d68c3b858dec9ad211665ccbcbf
Dirk Arnez <smalldirkalex@gmail.com>
2025-06-03 23:25:28 +0800

    - update .tbuild/codegen.py

--- .tbuild/codegen.py
+++ .tbuild/codegen.py
@@ -1,1 +1,11 @@
+# import cgen as c
 
+
+# func = c.FunctionBody(
+#     c.FunctionDeclaration(c.Const(c.Pointer(c.Value("char", "greet"))), []),
+#     c.Block([
+#         c.Statement('return "hello world"')
+#         ])
+#     )
+
+# print(func)
