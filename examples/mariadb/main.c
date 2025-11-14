635f347bb7fa8408cdfc32856134e44804301ef7
Dirk Arnez <smalldirkalex@gmail.com>
2025-11-14 15:14:20 +0800

    - update examples/mariadb/main.c

--- examples/mariadb/main.c
+++ examples/mariadb/main.c
@@ -1,1 +1,18 @@
 //[dirkarnez/mariadb-connector-c-playground](https://github.com/dirkarnez/mariadb-connector-c-playground/tree/main)
+/*
+   char query[1024];
+   strcpy(query, "SELECT * FROM information_schema.tables WHERE `TABLE_SCHEMA` = ?");
+
+   char data[255];
+   strcpy(data, "sys");
+   
+   MYSQL_BIND my_bind[1];
+   memset(my_bind, '\0', sizeof(my_bind));
+   
+   my_bind[0].buffer_type= MYSQL_TYPE_STRING;
+   my_bind[0].buffer= (void *)&data;
+   my_bind[0].buffer_length= strlen(data);
+   
+   // should be 102
+   fprintf(stdout, "%d", my_stmt_result(conn, query, my_bind));
+*/
